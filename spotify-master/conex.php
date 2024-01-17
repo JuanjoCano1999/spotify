d05d7e7011b55031c0490699d7dc857418c0eef6
HP196\CES <informatica@escuelaces.com>
2024-01-17 13:11:47 +0100

    Version7.0

--- spotify-master/conex.php
+++
@@ -1,8 +0,0 @@
-<?php   
-    $conex = mysqli_connect("localhost", "root", "","spotify");
-    if($conex->connect_error) {
-        die("error" . $conex->connect_error);
-    } else  {
-        echo "conectado";
-    }
-?>
